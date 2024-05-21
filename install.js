14b11b4a77cb175fe96d295d37f28d6db00008c0
Özgur Celik <ozce2300@student.miun.se>
2024-05-21 14:59:03 +0200

    Skapat tabell, med både register och login som funkar. Har även lagt till hash

--- install.js
+++ install.js
@@ -1,9 +1,26 @@
 require("dotenv").config();
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const sqlite3 = require("sqlite3");
 
 // Connect
 const db = new sqlite3.Database(process.env.Database);
 
-//create table
+//create table users
+
+db.serialize(() => {
+    //Drop if table exist
+
+    db.run("DROP TABLE IF EXISTS users");
+
+    //Create table
+
+    db.run(`CREATE TABLE users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(240) NOT NULL UNIQUE,
+        password VARCHAR(240) NOT NULL,
+        created DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`);
+
+    console.log("Table created")
+})
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,12 @@
 const express = require ("express")
 const router = express.Router();
+const sqlite3 = require("sqlite3");
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+
+
+// Connect to database
+const db = new sqlite3.Database(process.env.Database);
 
 // Add new user
 router.post("/register", async (req, res) => {
@@ -10,8 +17,19 @@ router.post("/register", async (req, res) => {
         if(!username || !password) {
             return res.status(400).json({error:("Invalid input, send username/password")})
         }
+        //Hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        
         //Correct - save user
-        res.status(201).json({message : "User created"})
+        const sql = `INSERT INTO users(username, password) VALUES(?,?)`;
+        db.run(sql, [username, hashedPassword], (err) =>{
+            if(err){
+                res.status(400).json({message : "error creating user"})
+            }
+            else {
+                res.status(201).json({message : "User created"})
+            }
+        })
     }
 
     catch(error) {
@@ -28,19 +46,34 @@ router.post ("/login", async(req, res) => {
         if(!username || !password) {
             return res.status(400).json({error:("Invalid input, send username/password")})
         }
+        //Check if user exist
+        const sql = `SELECT * FROM users WHERE username=?`;
+        db.get(sql, [username], async (err, row) => {
+            if(err) {
+                res.status(400).json({message : "Error from authenticating"})
+            }
 
-        //check credantials
-        if(username === "Rojva" && password==="password"){
-            res.status(200).json({message: "login succesfull"})
-        }
-        else {
-            res.status(401).json({error: "Invalid username/password"})
-        }
-    }
+            else if (!row) {
+                res.status(401).json({message: "Incorrect username/password"})
+            }
+            else {
+                //User exists
+                const passwordMatched = await bcrypt.compare(password, row.password)
 
-    catch (error) {
+                if(!passwordMatched) {
+                    res.status(401).json({message : "Incorret username/password"});
+                }
+
+                else {
+                    //correct login
+                    res.status(200).json({message: "Correct login"});
+                }
+            }
+        })
+
+    } catch (error) {
         res.status(500).json({ error : "server error"})
     }
-})
+});
 
 module.exports = router;
